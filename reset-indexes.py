e5ff163cce16f0c949b1d48058301f2789b60154
Will Urbanski <will.urbanski@gmail.com>
2015-12-30 12:24:29 -0500

    Reset indexes with appropriate mappings

---
+++ reset-indexes.py
@@ -0,0 +1,38 @@
+from elasticsearch import Elasticsearch
+from elasticsearch.client import IndicesClient
+import logging
+es = Elasticsearch()
+
+indices_delete = ['entrances']
+
+for index in indices_delete:
+  if es.indices.exists(index):
+    logging.warning("Deleteing index %s", index)
+    es.indices.delete(index=index, ignore=[400, 404])
+
+
+settings = {
+"settings": {
+  "dynamic": False,
+},
+"mappings": {
+  "entrances": 
+  {
+    "properties": 
+    {
+      "name": 
+      {
+        "type": "string"
+      },
+      "location":
+      {
+        "type": "geo_point",
+        "lat_lon": True
+      }
+    }
+  }
+}
+}
+es.indices.create(index='entrances', body=settings)
+
+es.index(index='entrances', doc_type='entrance', body={"name":"test","location":{'lat':0, 'lon':0}})
